f26e1c5b8cf5313220cac9409d567ae485991128
jwkoegeboehn <jwkoegeboehn@ksu.edu>
2020-01-29 14:17:59 -0600

    Ribs class done

---
+++ Data/Rustler'sRibs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    class Rustler_sRibs
+    {
+        public double Price
+        {
+            get
+            {
+                return 7.50;
+            }
+        }
+
+        public uint Calories
+        {
+            get
+            {
+                return 894;
+            }
+        }
+    }
+}
