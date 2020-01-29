822b2a34fe9a8c84521c48a98ab143f85788d330
jwkoegeboehn <jwkoegeboehn@ksu.edu>
2020-01-29 14:50:49 -0600

    Trailburger class done (w/o comments)

---
+++ Data/Trailburger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class Trailburger
+    {
+        public bool Ketchup { get; set; } = true;
+
+        public bool Mustard { get; set; } = true;
+
+        public bool Pickle { get; set; } = true;
+
+        public bool Cheese { get; set; } = true;
+
+        public double Price
+        {
+            get
+            {
+                return 4.50;
+            }
+        }
+
+        public uint Calories
+        {
+            get
+            {
+                return 288;
+            }
+        }
+
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                if (!Ketchup)
+                    instructions.Add("hold ketchup");
+                if (!Mustard)
+                    instructions.Add("hold mustard");
+                if (!Pickle)
+                    instructions.Add("hold pickle");
+            }
+        }
+    }
+}
