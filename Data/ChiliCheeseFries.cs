febac70db96a1d7cbac42fefd2f544b1968c177f
jwkoegeboehn <jwkoegeboehn@ksu.edu>
2020-02-03 14:52:54 -0600

    Pulled from master and made new side classes

---
+++ Data/ChiliCheeseFries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class ChiliCheeseFries : Side
+    {
+        public override Size Size
+        {
+            get { return Size.Small; }
+        }
+
+        public override double Price
+        {
+            get { return 1.99; }
+        } 
+
+        public override uint Calories
+        {
+            get { return 433; }
+        }
+    }
+}
