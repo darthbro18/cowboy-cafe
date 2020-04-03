fd7708806ba03e702e7303db62c4e4af2cb2f398
Justin Koegeboehn <jwbean18@gmail.com>
2020-04-03 15:13:45 -0500

    Pay by credit mostly complete. Total amount now displays

---
+++ PointOfSale/SubtotalToTotalConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace PointOfSale
+{
+    public class SubtotalToTotalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object param, CultureInfo culture)
+        {
+            return (double)value * 1.16;
+        }
+
+        public object ConvertBack(object value, Type targetType, object param, CultureInfo culture)
+        {
+            return (double)value / 1.16;
+        }
+    }
+}
--- PointOfSale/TransactionControl.xaml.cs
+++ PointOfSale/TransactionControl.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CashRegister;
+using CowboyCafe.Data;
 
 namespace PointOfSale
 {
@@ -17,14 +19,28 @@ namespace PointOfSale
     /// Interaction logic for TransactionControl.xaml
     /// </summary>
     public partial class TransactionControl : UserControl
-    {
+    {        
+        public double Total
+        {
+            get
+            {
+                string price = orderSummary.Subtotal.Text.Remove(0, 1);
+                return Convert.ToDouble(price) * 1.16;
+            }
+        }
+        
         public TransactionControl()
         {
             InitializeComponent();
+            //TotalTextBox.Text = String.Format("{0:C2}", Total);
         }
 
         public void OnCreditPayment(object sender, RoutedEventArgs e)
         {
+
+            CardTerminal ct = new CardTerminal();
+            ResultCode message = ct.ProcessTransaction(Total);
+            MessageBox.Show(message.ToString());
             orderSummary.IsEnabled = true;
             TransactionContainer.Child = new OrderControl();
         }
