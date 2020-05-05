352114c651a3d0ad9ef5b2d90474c55bb479a4c2
Justin Koegeboehn <jwbean18@gmail.com>
2020-05-05 14:51:47 -0500

    Search and Filter now uses LINQ

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -22,27 +22,27 @@ namespace Website.Pages
         public IEnumerable<IOrderItem> Items { get; protected set; }
         
         //Displayed items must contain the search terms in the item name
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public string SearchTerms { get; set; }
 
         //Filter for what categories to show
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public string[] Categories { get; set; }
 
         //Filter for min calories
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public int? CaloriesMin { get; set; }
 
         //Filter for max calories
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public int? CaloriesMax { get; set; }
 
         //Filter for min price
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public double? PriceMin { get; set; }
 
         //Filter for max price
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public double? PriceMax { get; set; }
         
         /// <summary>
@@ -54,19 +54,66 @@ namespace Website.Pages
         /// <param name="CaloriesMax"></param>
         /// <param name="PriceMin"></param>
         /// <param name="PriceMax"></param>
-        public void OnGet(string SearchTerms, string[] Categories, int? CaloriesMin, int? CaloriesMax, double? PriceMin, double? PriceMax)
+        public void OnGet()
         {
-            this.SearchTerms = SearchTerms;
-            this.Categories = Categories;
-            this.CaloriesMin = CaloriesMin;
-            this.CaloriesMax = CaloriesMax;
-            this.PriceMin = PriceMin;
-            this.PriceMax = PriceMax;
-
-            Items = Menu.Search(Menu.All, SearchTerms);
+            /*Items = Menu.Search(Menu.All, SearchTerms);
             Items = Menu.FilterByCategory(Items, Categories);
             Items = Menu.FilterByCalories(Items, CaloriesMin, CaloriesMax);
-            Items = Menu.FilterByPrice(Items, PriceMin, PriceMax);
+            Items = Menu.FilterByPrice(Items, PriceMin, PriceMax);*/
+
+            Items = Menu.All;
+            //Search menu items for SearchTerms
+            if (SearchTerms != null)
+            {
+                Items = from item in Items
+                        where item.ToString().Contains(SearchTerms, StringComparison.InvariantCultureIgnoreCase)
+                        select item;
+            }
+            //Filter by Category
+            if (Categories != null && Categories.Count() != 0)
+            {
+                Items = from item in Items
+                        where (item is Entree && Categories.Contains("Entrees")) || (item is Side && Categories.Contains("Sides")) || (item is Drink && Categories.Contains("Drinks"))
+                        select item;
+            }
+            //Filter by Calories
+            if (CaloriesMin == null && CaloriesMax != null)
+            {
+                Items = from item in Items
+                        where item.Calories <= CaloriesMax
+                        select item;
+            }
+            if (CaloriesMin != null && CaloriesMax == null)
+            {
+                Items = from item in Items
+                        where item.Calories >= CaloriesMin
+                        select item;
+            }
+            if (CaloriesMin != null && CaloriesMax != null)
+            {
+                Items = from item in Items
+                        where item.Calories >= CaloriesMin && item.Calories <= CaloriesMax
+                        select item;
+            }
+            //Filter by Price
+            if (PriceMin == null && PriceMax != null)
+            {
+                Items = from item in Items
+                        where item.Price <= PriceMax
+                        select item;
+            }
+            if (PriceMin != null && PriceMax == null)
+            {
+                Items = from item in Items
+                        where item.Price >= PriceMin
+                        select item;
+            }
+            if (PriceMin != null && PriceMax != null)
+            {
+                Items = from item in Items
+                        where item.Price >= PriceMin && item.Price <= PriceMax
+                        select item;
+            }
         }
     }
 }
